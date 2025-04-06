1f3a2ac77c3294e63101a4cc650affea5edada38
yordan-gergov01 <yordansoftskills@gmail.com>
2025-04-06 10:08:38 +0300

    creating logic to send data to all available clients in chat room

--- client.js
+++ client.js
@@ -1,3 +1,26 @@
 const net = require("net");
+const readline = require("readline/promises");
 
-net.createConnection();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const socket = net.createConnection(
+  { host: "127.0.0.1", port: 3008 },
+  async () => {
+    console.log("Connected to the server!");
+
+    const message = await rl.question("Enter a message >");
+
+    socket.write(message);
+  }
+);
+
+socket.on("data", (data) => {
+  console.log(data.toString("utf-8"));
+});
+
+socket.on("end", () => {
+  console.log("Connection was ended!");
+});
--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const net = require("net");
 
-net.createServer();
+const server = net.createServer();
+
+// sockets are endpoints that can only communicate to each other, point A and B
+// this way we keep track of the different clients
+// every time someone sends a message we will check the array and send it to each client
+// so everyone in the chat room can see it
+const clients = [];
+
+server.on("connection", (socket) => {
+  console.log("A new connection to the server!");
+
+  socket.on("data", (data) => {
+    clients.map((socket) => {
+      socket.write(data);
+    });
+  });
+
+  clients.push(socket);
+});
+
+server.listen(3008, "127.0.0.1", () => {
+  console.log("opened server on", server.address());
+});
